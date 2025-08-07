dab58e4b5d9cd212ea23763cfca5b803d1f4d2e3
shihuaxie <sylvia501628@gmail.com>
2025-08-07 13:00:44 +0000

    Add task 13 to count the nodes with at least 1 child in a binary tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts nodes with at least one child
+ * @tree: Pointer to root node
+ *
+ * Return: Number of nodes with at least 1 child
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	/** post-order traversal: left right root */
+	if (tree->left != NULL || tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	else
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
